c1895ec586dd3bbcfb271ac456261ca13f7f7f72
Ciprian Serbu <ciprian.serbu@endava.com>
2020-09-15 15:05:17 +0300

    initial commit with files from generator library

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = {
+    download: require('./lib/download.js'),
+    validate: require('./lib/validate.js'),
+    generate: require('./lib/generate.js'),
+    github: require('./lib/github.js'),
+    catalog: require('./lib/catalog.js'),
+    util: require('./lib/util.js'),
+};
